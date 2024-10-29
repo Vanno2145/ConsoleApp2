e27375c12bb6e5772e721c1728e3663335f9ad1e
ivanm <ivanm@DESKTOP-F33GBDN>
2024-10-29 19:36:32 +0300

    lgh

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,131 +2,205 @@
 
 class Program
 {
+    public class Matrix
+    {
+        private int[,] matrix;
+
+        public Matrix(int rows, int columns)
+        {
+            matrix = new int[rows, columns];
+        }
+
+        public int Rows { get { return matrix.GetLength(0); } }
+        public int Columns { get { return matrix.GetLength(1); } }
+
+        public int this[int row, int col]
+        {
+            get { return matrix[row, col]; }
+            set { matrix[row, col] = value; }
+        }
+
+        public void Input()
+        {
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    Console.Write($"Введите елемент [{i}, {j}]: ");
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+
+        public void PrintMatrix()
+        {
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    Console.Write($"{matrix[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        
+        public int FindMaxElement()
+        {
+            int max = matrix[0, 0];
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    if (matrix[i, j] > max)
+                    {
+                        max = matrix[i, j];
+                    }
+                }
+            }
+            return max;
+        }
+
+        public int FindMinElement()
+        {
+            int min = matrix[0, 0];
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    if (matrix[i, j] < min)
+                    {
+                        min = matrix[i, j];
+                    }
+                }
+            }
+            return min;
+        }
+
+        public static Matrix operator +(Matrix a, Matrix b)
+        {
+            if (a.Rows != b.Rows || a.Columns != b.Columns)
+            {
+                throw new ArgumentException("Для сложения матрицы должны иметь одинаковые размеры");
+            }
+
+            Matrix result = new Matrix(a.Rows, a.Columns);
+            for (int i = 0; i < a.Rows; i++)
+            {
+                for (int j = 0; j < a.Columns; j++)
+                {
+                    result[i, j] = a[i, j] + b[i, j];
+                }
+            }
+            return result;
+        }
+        public static Matrix operator -(Matrix a, Matrix b)
+        {
+            if (a.Rows != b.Rows || a.Columns != b.Columns)
+            {
+                throw new ArgumentException("Для сложения матрицы должны иметь одинаковые размеры");
+            }
+
+            Matrix result = new Matrix(a.Rows, a.Columns);
+            for (int i = 0; i < a.Rows; i++)
+            {
+                for (int j = 0; j < a.Columns; j++)
+                {
+                    result[i, j] = a[i, j] - b[i, j];
+                }
+            }
+            return result;
+        }
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+            if (a.Rows != b.Rows || a.Columns != b.Columns)
+            {
+                throw new ArgumentException("Для сложения матрицы должны иметь одинаковые размеры");
+            }
+
+            Matrix result = new Matrix(a.Rows, a.Columns);
+            for (int i = 0; i < a.Rows; i++)
+            {
+                for (int j = 0; j < a.Columns; j++)
+                {
+                    result[i, j] = a[i, j] * b[i, j];
+                }
+            }
+            return result;
+        }
+        public static string operator ==(Matrix a, Matrix b)
+        {
+            if (a.Rows != b.Rows || a.Columns != b.Columns)
+            {
+                throw new ArgumentException("Матрицы разные");
+            }
+
+            Matrix result = new Matrix(a.Rows, a.Columns);
+            int c = -1;
+            for (int i = 0; i < a.Rows; i++)
+            {
+                for (int j = 0; j < a.Columns; j++)
+                {
+                    if (a[i, j] == b[i, j])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        c = 1;
+                        throw new ArgumentException("Матрицы разные");
+                    }
+                }
+            }
+            if (c == -1)
+            {
+                return "Матрицы равны";
+            }
+            else
+            {
+                throw new ArgumentException("Матрицы разные");
+            }
+          
+        }
+        public static string operator !=(Matrix a, Matrix b)
+        {
+            if (a.Rows != b.Rows || a.Columns != b.Columns)
+            {
+                Console.WriteLine("Матрицы разные");
+            }
+
+            Matrix result = new Matrix(a.Rows, a.Columns);
+            int c = -1;
+            for (int i = 0; i < a.Rows; i++)
+            {
+                for (int j = 0; j < a.Columns; j++)
+                {
+                    if (a[i, j] == b[i, j])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        c = 1;
+                        throw new ArgumentException("Матрицы разные");
+                    }
+                }
+            }
+            if (c == -1)
+            {
+                return"Матрицы равны";
+            }
+            else
+            {
+                throw new ArgumentException("Матрицы разные");
+            }
+
+        }
+    }
+
     static void Main(string[] args)
     {
-        //while (true)
-        //{
-        //    Console.Clear();
-        //    Console.WriteLine("Калькулятор для перевода чисел между системами счисления");
-        //    Console.WriteLine("1. Десятичная -> Двоичная");
-        //    Console.WriteLine("2. Двоичная -> Десятичная");
-        //    Console.WriteLine("3. Десятичная -> Шестнадцатеричная");
-        //    Console.WriteLine("4. Шестнадцатеричная -> Десятичная");
-        //    Console.WriteLine("5. Выход");
-        //    Console.Write("Ваш выбор: ");
 
-        //    string menuChoice = Console.ReadLine();
-
-        //    if (menuChoice == "5") break;
-
-        //    Console.Write("Введите число: ");
-        //    string userInput = Console.ReadLine();
-
-        //    try
-        //    {
-        //        int number;
-
-        //        if (menuChoice == "1")
-        //        {
-        //            number = int.Parse(userInput);
-        //            Console.WriteLine($"Двоичное представление: {Convert.ToString(number, 2)}");
-        //        }
-        //        else if (menuChoice == "2")
-        //        {
-        //            number = Convert.ToInt32(userInput, 2);
-        //            Console.WriteLine($"Десятичное представление: {number}");
-        //        }
-        //        else if (menuChoice == "3")
-        //        {
-        //            number = int.Parse(userInput);
-        //            Console.WriteLine($"Шестнадцатеричное представление: {number:X}");
-        //        }
-        //        else if (menuChoice == "4")
-        //        {
-        //            number = Convert.ToInt32(userInput, 16);
-        //            Console.WriteLine($"Десятичное представление: {number}");
-        //        }
-        //        else
-        //        {
-        //            Console.WriteLine("Неверный выбор.");
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Console.WriteLine($"Ошибка: {ex.Message}");
-        //    }
-
-        //    Console.WriteLine("Нажмите любую клавишу для продолжения...");
-        //    Console.ReadKey();
-        //}
-
-        //Task 2
-    }
-    public class Passport{
-
-        private string number;
-        private string owner;
-        private DateTime issueDate;
-        private DateTime expirationDate;
-
-        public string Number
-        {
-            get { return number; }
-            set
-            {
-                if (value == null || value.Length != 9)
-                    throw new ArgumentException("Номер паспорта должен состоять из 9 символов.");
-                number = value;
-            }
-        }
-
-        public string Owner
-        {
-            get { return owner; }
-            set
-            {
-                if (value == null || value == "")
-                    throw new ArgumentException("ФИО владельца не может быть пустым.");
-                owner = value;
-            }
-        }
-
-        public DateTime IssueDate
-        {
-            get { return issueDate; }
-            set
-            {
-                if (value > DateTime.Now)
-                    throw new ArgumentException("Дата выдачи не может быть в будущем.");
-                issueDate = value;
-            }
-        }
-
-        public DateTime ExpirationDate
-        {
-            get { return expirationDate; }
-            set
-            {
-                if (value <= issueDate)
-                    throw new ArgumentException("Дата истечения срока должна быть позже даты выдачи.");
-                expirationDate = value;
-            }
-        }
-
-        public Passport(string number, string owner, DateTime issueDate, DateTime expirationDate)
-        {
-            Number = number;
-            Owner = owner;
-            IssueDate = issueDate;
-            ExpirationDate = expirationDate;
-        }
-
-        public override string ToString()
-        {
-            return $"Паспорт: {Number}, Владелец: {Owner}, Дата выдачи: {IssueDate.ToShortDateString()}, " +
-                    $"Дата истечения: {ExpirationDate.ToShortDateString()}";
-        }
-    }
+    }       
 }
 
 
